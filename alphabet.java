e02e9f65fae7975d540381d60a63f87ef4360fa4
imrozpatan <105908753+imrozpatan@users.noreply.github.com>
2022-06-09 15:39:59 +0530

    Sample File added

---
+++ alphabet.java
@@ -0,0 +1,9 @@
+class  alphabet
+{
+	public static void main(String[] args) 
+	{
+		char ch = 6;
+		if(ch>='a'&&ch<='z'||ch>'A'&&ch<'Z');
+		System.out.println(ch+" given value is alphabet");
+	}
+}
